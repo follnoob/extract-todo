8dcf850bdb2d5548cdf31e1615b8a704dfe24b02
Jens Wilberg <jens_wilberg@outlook.com>
2017-08-08 11:16:25 +0200

    Added project url

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/follnoob/',
+    url='https://github.com/follnoob/extrect-todo',
 
     # Author details
     author='Jens Wilberg',
