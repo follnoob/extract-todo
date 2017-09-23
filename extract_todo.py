7010961bf4e86aa6e535af8dba85a4c8092e05e3
Jens Wilberg <jens_wilberg@outlook.com>
2017-09-23 19:25:25 +0200

    Bump version: 0.1.0 -> 0.1.1

--- extract_todo.py
+++ extract_todo.py
@@ -23,7 +23,7 @@ import codecs
 import os
 import argparse
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # GLOBALS
 _FILES = {
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='extract-todo',
-    version="0.1.0",
+    version="0.1.1",
 
     description='extract TODOs',
     long_description=long_description,
