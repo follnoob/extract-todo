0807e6b8358a39cd0e73fbe7108ca88a80d1fd09
Jens Wilberg <jens_wilberg@outlook.com>
2019-11-11 21:58:22 +0100

    Add parser classes

---
+++ extract_todo/parser/default_parser.py
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2017 - 2019 Jens Wilberg <jens_wilberg@outlook.com>
+#
+# This file is part of extract-todo.
+#
+# extract-todo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# extract-todo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with extract-todo.  If not, see <http://www.gnu.org/licenses/>.
+"""This module contains the default parser for files."""
+import re
+
+
+class DefaultParser:
+    """Defaultparser class.
+
+    This class represents a default parser, which can parse all files wit '//'
+    as single line comment chars.
+
+    Parameters
+    ----------
+    fname : str
+        filename
+    """
+
+    def __init__(self, fname: str):
+        self._comment = r"//.+"
+        self._file = fname
+
+    def parse(self) -> list:
+        """Parse file for comments.
+
+        Returns
+        -------
+        list of tuple
+            List of tuples. the tuples have the form
+            (filename, linenumber, string)
+        """
+        comments = []
+        with open(self._file, "r") as f:
+            for line, text in enumerate(f, 1):
+                match = re.search(self._comment, text)
+                if match:
+                    comment = match.group(0)[2:].strip()
+                    comments.append((self._file, line, comment))
+
+        return comments
---
+++ extract_todo/parser/parser_factory.py
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2017 - 2019 Jens Wilberg <jens_wilberg@outlook.com>
+#
+# This file is part of extract-todo.
+#
+# extract-todo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# extract-todo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with extract-todo.  If not, see <http://www.gnu.org/licenses/>.
+"""This module contains the parser factory."""
+import os
+
+from .default_parser import DefaultParser
+from .python_parser import PythonParser
+from .latex_parser import LatexParser
+
+
+class ParserFactory():
+
+    def __init__(self):
+        self._builders = {
+            # Files with default parser
+            ".h": DefaultParser,    # C/C++
+            ".c": DefaultParser,    # C
+            ".hpp": DefaultParser,  # C++
+            ".cpp": DefaultParser,  # C++
+            ".js": DefaultParser,   # Javascript
+            # Files with custom parser
+            ".tex": LatexParser,    # Latex
+            ".py": PythonParser     # Python
+        }
+
+    def create(self, fname: str):
+        """Create a appropriated parser for the file."""
+        if os.path.isdir(fname):
+            raise ValueError("'{}' is a folder!".format(fname))
+        file_extension = os.path.splitext(fname)[1]
+        builder = self._builders.get(file_extension)
+        if not builder:
+            raise ValueError(
+                "'{}'-files are not supported!".format(file_extension))
+        return builder(fname)
