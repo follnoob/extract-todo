690237bc54bf2a1069841a7f82fd85ec2d41e60a
Jens Wilberg <jens_wilberg@outlook.com>
2019-11-11 21:54:05 +0100

    Transform module to package

---
+++ extract_todo/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa: F401
+"""Python package initialization."""
+from .__version__ import VERSION_STRING as __version__
+from .extractor import extract_todos
---
+++ extract_todo/__main__.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Copyright (C) 2017 - 2019 Jens Wilberg <jens_wilberg@outlook.com>
+#
+# This file is part of extract-todo.
+#
+# extract-todo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# extract-todo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with extract-todo.  If not, see <http://www.gnu.org/licenses/>.
+"""Tool for extracting TODOs from text/source files.
+
+Currently only 'utf-8' file-encoding is supported.
+"""
+import os
+import argparse
+
+from extract_todo.__version__ import VERSION_STRING
+from extract_todo.extractor import extract_todos
+
+__version__ = VERSION_STRING
+
+
+def main():
+    """Main function as entry point."""
+    parser = argparse.ArgumentParser(prog="extract-todo", description="Extracts\
+            TODOs from a given file. TODOs are only recognized, if there are at\
+            the beginning of a single line comment. Supported files are LaTex\
+            tex-files and Python-files. Currently only 'utf-8' file-encoding is\
+            supported.")
+    parser.add_argument("fname", metavar="FILENAME", help="Path to file.")
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s {}'.format(__version__))
+    args = parser.parse_args()
+    fname = args.fname
+    try:
+        todos = extract_todos(fname)
+    except KeyError:
+        ext = os.path.splitext(fname)
+        print("%s-files are not supported!" % ext[1])
+        exit(1)
+    if todos:
+        print('\n'.join(todos))
+    else:
+        print("There are no TODOs.")
+
+
+if __name__ == "__main__":
+    main()
