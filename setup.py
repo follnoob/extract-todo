d6479392d2841f6ed5a00a2ee1a2b60a7d70ba76
Jens Wilberg <jens_wilberg@outlook.com>
2019-11-11 21:59:56 +0100

    Change compatible Python3 versions

--- setup.py
+++ setup.py
@@ -48,9 +48,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here.
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     # What does your project relate to?
